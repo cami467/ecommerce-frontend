3587bc9e3d6caa244733349f607a596e1f4380fe
Camila Benitez <camibenitez61@gmail.com>
2026-06-30 11:05:29 -0300

    feat: integra Header en App junto a las rutas

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import { Header } from './components/layout/Header'
 import { ProtectedRoute } from './routes/ProtectedRoute'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
@@ -6,14 +7,17 @@ import MiCuentaPage from './pages/MiCuentaPage'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
 
-      <Route element={<ProtectedRoute />}>
-        <Route path="/mi-cuenta" element={<MiCuentaPage />} />
-      </Route>
-    </Routes>
+        <Route element={<ProtectedRoute />}>
+          <Route path="/mi-cuenta" element={<MiCuentaPage />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
 
