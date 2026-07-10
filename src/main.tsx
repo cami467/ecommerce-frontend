0be687e694dc6b288ab641b1cf8fbbc48ef8fa85
Camila Benitez <camibenitez61@gmail.com>
2026-07-10 17:21:29 -0300

    feat(frontend): integrar NotificacionesProvider en main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import App from './App.tsx'
+import { NotificacionesProvider } from './context/NotificacionesContext' 
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,7 +19,9 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <App />
+        <NotificacionesProvider>
+          <App />
+        </NotificacionesProvider>
       </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>,
